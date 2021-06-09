f3d69cec28f8e7716fbd40e4c47b2b3089203a4e
Aviv Haruzi <avivharuzi@gmail.com>
2021-06-09 15:36:42 +0300

    Add EmailTemplateService

--- server/src/modules/shared/email-template.service.ts
+++ server/src/modules/shared/email-template.service.ts
@@ -1,19 +1,34 @@
 import { sendEmailWithTemplate } from '../../utils/send-email-with-template';
 
-export interface UserVerificationContext {
-  firstName: string;
-  lastName: string;
+export interface UserVerificationEmailContext {
+  userName: string;
+  link: string;
+}
+
+export interface PasswordResetEmailContext {
+  userName: string;
   link: string;
 }
 
 export class EmailTemplateService {
   static async sendUserVerification(
     to: string,
-    context: UserVerificationContext
+    context: UserVerificationEmailContext
   ): Promise<void> {
-    return sendEmailWithTemplate<UserVerificationContext>(
+    return sendEmailWithTemplate<UserVerificationEmailContext>(
       to,
       'user-verification',
+      context
+    );
+  }
+
+  static async sendPasswordReset(
+    to: string,
+    context: PasswordResetEmailContext
+  ): Promise<void> {
+    return sendEmailWithTemplate<PasswordResetEmailContext>(
+      to,
+      'password-reset',
       context
     );
   }
