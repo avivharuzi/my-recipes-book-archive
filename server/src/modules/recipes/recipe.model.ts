77841488c7b241624dece55438eb0bccce68dbd7
Aviv Haruzi <avivharuzi@gmail.com>
2021-06-10 12:49:07 +0300

    Add recipe model

---
+++ server/src/modules/recipes/recipe.model.ts
@@ -0,0 +1,37 @@
+import { Document, model, PopulatedDoc, Schema } from 'mongoose';
+
+import { Image } from '../images/image.model';
+import { User } from '../users/user.model';
+
+export interface Recipe extends Document {
+  user: PopulatedDoc<User & Document>;
+  title: string;
+  slug: string;
+  description: string;
+  ingredients: string[];
+  directions: string[];
+  coverImage?: PopulatedDoc<Image & Document>;
+  preparationTime: number;
+  cookingTime: number;
+  servingsAmount: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const recipeSchema = new Schema<Recipe>(
+  {
+    user: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
+    title: { type: String, required: true, trim: true },
+    slug: { type: String, required: true, trim: true },
+    description: { type: String, required: true, trim: true },
+    ingredients: [{ type: String, trim: true }],
+    directions: [{ type: String, trim: true }],
+    coverImage: { type: Schema.Types.ObjectId, ref: 'Image' },
+    preparationTime: { type: Number, required: true },
+    cookingTime: { type: Number, required: true },
+    servingsAmount: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
+
+export const RecipeModel = model<Recipe>('Recipe', recipeSchema, 'recipes');
