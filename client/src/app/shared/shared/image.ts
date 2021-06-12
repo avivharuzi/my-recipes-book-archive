6ae50609ef0752a43b818ab893f61861679581fd
Aviv Haruzi <avivharuzi@gmail.com>
2021-06-12 20:17:26 +0300

    Add image interface

---
+++ client/src/app/shared/shared/image.ts
@@ -0,0 +1,22 @@
+export interface Image {
+  sizes: ImageSizes;
+  description?: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface ImageSizes {
+  thumbnail: ImageSize;
+  small: ImageSize;
+  medium: ImageSize;
+  large: ImageSize;
+  original: ImageSize;
+}
+
+export interface ImageSize {
+  path: string;
+  width: number;
+  height: number;
+  mimeType: string;
+  size: number;
+}
