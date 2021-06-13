195cb7b9f49e4d4ce512a99b9a9319ab43ba685f
Aviv Haruzi <avivharuzi@gmail.com>
2021-06-13 13:19:43 +0300

    Add form input error component

---
+++ client/src/app/shared/components/form-input-error/form-input-error.component.ts
@@ -0,0 +1,48 @@
+import { AbstractControl } from '@angular/forms';
+import { AfterContentChecked, Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-form-input-error',
+  templateUrl: './form-input-error.component.html',
+  styleUrls: ['./form-input-error.component.scss'],
+})
+export class FormInputErrorComponent implements AfterContentChecked {
+  @Input() isMulti: boolean;
+  @Input() control: AbstractControl | null;
+
+  errors: string[];
+
+  constructor() {
+    this.isMulti = false;
+    this.control = null;
+    this.errors = [];
+  }
+
+  ngAfterContentChecked(): void {
+    this.reset();
+
+    if (!this.control) {
+      return;
+    }
+    if (!this.control.dirty || !this.control.invalid || !this.control.errors) {
+      return;
+    }
+
+    for (const key in this.control.errors) {
+      if (!this.control.errors.hasOwnProperty(key)) {
+        continue;
+      }
+      const error = this.control.errors[key];
+      if (error && typeof error === 'string') {
+        this.errors.push(error);
+        if (!this.isMulti) {
+          break;
+        }
+      }
+    }
+  }
+
+  private reset(): void {
+    this.errors = [];
+  }
+}
