f380017ab3e517e18394677f609cd52c489e1254
Aviv Haruzi <avivharuzi@gmail.com>
2021-06-13 15:26:19 +0300

    Add form input component

---
+++ client/src/app/shared/components/form-input/form-input.component.ts
@@ -0,0 +1,62 @@
+import { AfterViewInit, Component, forwardRef, Input } from '@angular/core';
+import {
+  AbstractControl,
+  ControlValueAccessor,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-form-input',
+  templateUrl: './form-input.component.html',
+  styleUrls: ['./form-input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => FormInputComponent),
+      multi: true,
+    },
+  ],
+})
+export class FormInputComponent implements ControlValueAccessor, AfterViewInit {
+  @Input() type: string;
+  @Input() id: string;
+  @Input() placeholder: string;
+  @Input() label: string;
+  @Input() describe: string;
+  @Input() describedby: string;
+  @Input() control: AbstractControl | null;
+
+  value: string;
+
+  constructor() {
+    this.type = 'text';
+    this.id = '';
+    this.placeholder = '';
+    this.label = '';
+    this.value = '';
+    this.describe = '';
+    this.describedby = '';
+    this.control = null;
+  }
+
+  ngAfterViewInit(): void {
+    this.value = this.control?.value;
+  }
+
+  onChange(_: Event, value: string): void {
+    this.value = value;
+    this.propagateChange(this.value);
+  }
+
+  writeValue(value: string): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.propagateChange = fn;
+  }
+
+  propagateChange = (_: any): void => {};
+
+  registerOnTouched(_: any): void {}
+}
