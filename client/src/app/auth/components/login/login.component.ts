c7d1c2da478c7446e3f747e96b62e9ee8880e653
Aviv Haruzi <avivharuzi@gmail.com>
2021-06-14 16:37:26 +0300

    Add login component

---
+++ client/src/app/auth/components/login/login.component.ts
@@ -0,0 +1,29 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ErrorMessage, Message } from '../../../shared/shared/message';
+
+import { AuthService } from '../../shared/auth.service';
+import { LoginBody } from '../../shared/login.body';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LoginComponent {
+  message?: Message;
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onFormSubmit(body: LoginBody): void {
+    this.authService.login(body).subscribe(
+      () => {
+        this.router.navigate(['/']).then();
+      },
+      error => {
+        this.message = new ErrorMessage(error.message);
+      }
+    );
+  }
+}
--- client/src/app/auth/components/resend-verification/resend-verification.component.ts
+++ client/src/app/auth/components/resend-verification/resend-verification.component.ts
@@ -1,17 +1,18 @@
-import { Component } from '@angular/core';
-
-import { AuthService } from '../../shared/auth.service';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import {
   ErrorMessage,
   Message,
   SuccessMessage,
 } from '../../../shared/shared/message';
+
+import { AuthService } from '../../shared/auth.service';
 import { ResendVerificationBody } from '../../shared/resend-verification-body';
 
 @Component({
   selector: 'app-resend-verification',
   templateUrl: './resend-verification.component.html',
   styleUrls: ['./resend-verification.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ResendVerificationComponent {
   message?: Message;
