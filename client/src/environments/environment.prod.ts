1d1f5c2c6db5bfa5a3c6f18c9ca59f779cf1a4e2
Aviv Haruzi <avivharuzi@gmail.com>
2021-06-14 10:31:33 +0300

    Add baseApiUrl

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   defaultImagePath: 'assets/images/defaults/default-image.png',
+  baseApiUrl: '/api',
   baseImagesApiUrl: 'https://myrecipesbook.s3.eu-central-1.amazonaws.com',
 };
