f61ab2da37ccd1d91291ba88c8ff9cb576a592a1
Aviv Haruzi <avivharuzi@gmail.com>
2021-06-14 11:50:52 +0300

    Add auth request bodies interfaces

---
+++ client/src/app/auth/shared/login.body.ts
@@ -0,0 +1,5 @@
+export interface LoginBody {
+  email?: string;
+  userName?: string;
+  password: string;
+}
---
+++ client/src/app/auth/shared/sign-up-body.ts
@@ -0,0 +1,7 @@
+export interface SignUpBody {
+  firstName: string;
+  lastName: string;
+  email: string;
+  userName: string;
+  password: string;
+}
