3f1487f99f339c4ee71e6d288b709bc87023a977
Aviv Haruzi <avivharuzi@gmail.com>
2021-06-14 16:30:12 +0300

    Add signup component

---
+++ client/src/app/auth/components/sign-up/sign-up.component.ts
@@ -0,0 +1,34 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+import {
+  ErrorMessage,
+  Message,
+  SuccessMessage,
+} from '../../../shared/shared/message';
+import { AuthService } from '../../shared/auth.service';
+import { SignUpBody } from '../../shared/sign-up-body';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SignUpComponent {
+  message?: Message;
+
+  constructor(private authService: AuthService) {}
+
+  onFormSubmit(body: SignUpBody): void {
+    this.authService.signUp(body).subscribe(
+      () => {
+        this.message = new SuccessMessage(
+          `Account created successfully. A verification email has been sent to ${body.email}.`
+        );
+      },
+      error => {
+        this.message = new ErrorMessage(error);
+      }
+    );
+  }
+}
