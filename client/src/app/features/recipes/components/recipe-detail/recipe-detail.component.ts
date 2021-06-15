027f6ae48ec4f9309df41937440b8630511e748d
Aviv Haruzi <avivharuzi@gmail.com>
2021-06-15 10:04:55 +0300

    Update recipe detail component

--- client/src/app/features/recipes/components/recipe-detail/recipe-detail.component.ts
+++ client/src/app/features/recipes/components/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,10 @@
+import { ActivatedRoute } from '@angular/router';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { mergeMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+import { Recipe } from '../../shared/recipe';
+import { RecipeService } from '../../shared/recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -6,4 +12,15 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./recipe-detail.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RecipeDetailComponent {}
+export class RecipeDetailComponent {
+  recipe$: Observable<Recipe>;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private recipeService: RecipeService
+  ) {
+    this.recipe$ = this.activatedRoute.paramMap.pipe(
+      mergeMap(params => this.recipeService.getDetail(params.get('id') || ''))
+    );
+  }
+}
