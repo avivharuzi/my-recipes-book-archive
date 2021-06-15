44da77ddcdfcc1640ed9c4d2697b870a91001afa
Aviv Haruzi <avivharuzi@gmail.com>
2021-06-15 16:15:44 +0300

    Add type to button component

--- client/src/app/shared/components/button/button.component.ts
+++ client/src/app/shared/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -6,4 +6,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./button.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ButtonComponent {}
+export class ButtonComponent {
+  @Input() type?: string;
+}
