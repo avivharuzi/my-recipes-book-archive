f99a4a5f4fb5774f3699c4e151c7e8b91ad16161
Aviv Haruzi <avivharuzi@gmail.com>
2021-06-16 09:16:45 +0300

    Update core components

--- client/src/app/core/core.component.ts
+++ client/src/app/core/core.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
@@ -9,6 +9,7 @@ import { User } from '../features/auth/shared/user';
   selector: 'app-core',
   templateUrl: './core.component.html',
   styleUrls: ['./core.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CoreComponent {
   user$: Observable<User | null>;
