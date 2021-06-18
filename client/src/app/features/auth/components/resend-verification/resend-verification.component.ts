ae031c51cb4a569f480b23a09af1b5178e13e055
Aviv Haruzi <avivharuzi@gmail.com>
2021-06-18 10:02:44 +0300

    Update resend verification component

--- client/src/app/features/auth/components/resend-verification/resend-verification.component.ts
+++ client/src/app/features/auth/components/resend-verification/resend-verification.component.ts
@@ -1,12 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import {
-  ErrorMessage,
-  Message,
-  SuccessMessage,
-} from '../../../../shared/shared/message';
 
 import { AuthService } from '../../shared/auth.service';
+import { errorMessageOperator } from '../../../../shared/shared/error-message-operator';
+import { Message, SuccessMessage } from '../../../../shared/shared/message';
 import { ResendVerificationBody } from '../../shared/resend-verification-body';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-resend-verification',
@@ -15,20 +13,24 @@ import { ResendVerificationBody } from '../../shared/resend-verification-body';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ResendVerificationComponent {
-  message?: Message;
+  message$: Subject<Message>;
+  isResendVerificationSuccessfully: boolean;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) {
+    this.message$ = new Subject<Message>();
+    this.isResendVerificationSuccessfully = false;
+  }
 
   onFormSubmit(body: ResendVerificationBody): void {
-    this.authService.resendVerification(body).subscribe(
-      () => {
-        this.message = new SuccessMessage(
-          `A verification email has been sent to ${body.email}.`
+    this.authService
+      .resendVerification(body)
+      .pipe(errorMessageOperator(message => this.message$.next(message)))
+      .subscribe(() => {
+        this.message$.next(
+          new SuccessMessage(
+            `A verification email has been sent to ${body.email}.`
+          )
         );
-      },
-      error => {
-        this.message = new ErrorMessage(error.message);
-      }
-    );
+      });
   }
 }
